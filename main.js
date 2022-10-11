f96de70ab52f22c7f0ce7061dc4ba98f9e567cc1
stellaris9950 <andyzihengwei@gmail.com>
2022-10-11 13:28:35 -0600

    start

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hi");
+// Array Visuallizer
+
+//Global Variable
+let myData = [400, 500, 250, 550, 100, 300];
+
+//Display Date
+drawArray();
+
+
+function drawArray() {
+    let outputStr = ""
+    for (let i = 0; i < myData.length; i++){
+        outputStr +=
+    }
+}
+
+
